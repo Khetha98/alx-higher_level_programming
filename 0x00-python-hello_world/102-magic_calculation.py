d1fc6e399e5e82fe4c745e114ceb80286ea93f73
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-10-31 04:45:47 +0200

    number 13

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,8 +1,9 @@
 import dis
-def maic_calculation(a, b):
+def magic_calculation(a, b):
     return ((98 + a)**b)
 
-expected_bytecode = ("  2           0 LOAD_CONST               1 (98)\n"
+expected_bytecode = (
+    "  2           0 LOAD_CONST               1 (98)\n"
     "              3 LOAD_FAST                0 (a)\n"
     "              6 LOAD_FAST                1 (b)\n"
     "              9 BINARY_POWER\n"
