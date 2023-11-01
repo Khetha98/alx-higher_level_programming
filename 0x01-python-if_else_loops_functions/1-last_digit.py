79d1eb0ddb2494602822931b94f2cb2a14339ee2
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-11-01 03:37:52 +0200

    decreased number of lines at file 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,14 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 # remainder = number % 10
 if number < 0:
-    remainder = number % -10
+    r = number % -10
 else:
-    remainder = number % 10
-if (remainder != 0 and remainder < 6):
-    print(f"Last digit of {number} is {remainder} and is less than 6 and not 0")
-elif (remainder == 0):
+    r = number % 10
+if (r != 0 and r < 6):
+    print(f"Last digit of {number} is {r} and is less than 6 and not 0")
+elif (r == 0):
     print(f"Last digit of {number} is 0 and is 0")
-elif (remainder != 0 and remainder > 6):
-    print(f"Last digit of {number} is {remainder} and is greater than 5")
-
-
+elif (r != 0 and r > 6):
+    print(f"Last digit of {number} is {r} and is greater than 5")
