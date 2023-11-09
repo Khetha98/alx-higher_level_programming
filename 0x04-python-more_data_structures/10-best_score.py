6b7580b9004ac103023dd5e8f857dfeed4385d34
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-11-09 03:41:36 +0200

    modified

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary:
         the_list = list(a_dictionary.keys())
-        l = ""
         s = 0
+        l = ""
         for i in the_list:
             if a_dictionary[i] > s:
                 s = a_dictionary[i]
                 l = i
-        return l
+        return (l)
