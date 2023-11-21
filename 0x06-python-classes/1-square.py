92b63a788dd2b6673b433cc0a0cebbaa757e4d90
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-11-21 19:17:22 +0200

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""Define the class Square."""
+
+
+class Square:
+    """Represent the square."""
+
+    def __init__(self, size):
+        """Initialize the new Square.
+
+        Args:
+            size (int): The size of a new square.
+        """
+        self.__size = size
