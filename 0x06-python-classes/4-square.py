ef166d9fda617f9470ddb38bbb64cb3f2d733784
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-11-21 20:27:49 +0200

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Define the class Square."""
+
+
+class Square:
+    """Represent the square."""
+
+    def __init__(self, size=0):
+        """Initialize the new square.
+
+        Args:
+            size (int): The size of a new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of a square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of a square."""
+        return (self.__size * self.__size)
