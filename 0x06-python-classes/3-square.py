a239c811ff786a3d969ae602258838646927132e
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-11-21 20:20:29 +0200

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# 0-square.py by Ehoneah Obed
+"""This module defines a square """
+
+
+class Square:
+    """This class represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing a square class
+        Args:
+            size: represnets size of a square defined
+        Raises:
+            TypeError: if the size is not an integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate area of a square
+        Returns: The square of size
+        """
+
+        return (self.__size ** 2)
+    
