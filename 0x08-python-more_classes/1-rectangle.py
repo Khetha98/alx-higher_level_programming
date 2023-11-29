19b88f03ab3a448b55ee76947bfd75510fd39093
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-11-30 00:53:42 +0200

    my work

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,12 +25,12 @@ class Rectangle:
 
         @property
         def width(self):
-            """get the width attribute"""
+            """retrieves width attribute"""
             return self.__width
 
         @width.setter
         def width(self, value):
-            """sets the width attribute"""
+            """sets width attribute"""
             if not isinstance(value, int):
                 raise TypeError("width must be an integer")
             if value < 0:
@@ -39,12 +39,12 @@ class Rectangle:
 
         @property
         def height(self):
-            """get the height attribute"""
+            """retrieves height attribute"""
             return self.__height
 
         @height.setter
         def height(self, value):
-            """sets the height attribute"""
+            """sets height attribute"""
             if not isinstance(value, int):
                 raise TypeError("height must be an integer")
             if value < 0:
