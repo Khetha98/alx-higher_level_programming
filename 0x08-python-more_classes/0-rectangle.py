64009a9a263979868d081c3d07122301c2cc316a
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-11-29 11:08:19 +0200

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""A class that gives an empty rectangle class"""
+
+class Rectangle:
+    """This is an empty rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This project delves more into classes on python
