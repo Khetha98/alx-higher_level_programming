032609f3296782eeafda587f4ddbdf16e677c0db
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-12-06 03:35:10 +0200

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This module gives the list of available attributes
+    and the methods of the object
+"""
+
+
+def lookup(obj):
+    """This functions looks for all attributes and methods of the object"""
+    return dir(obj)
