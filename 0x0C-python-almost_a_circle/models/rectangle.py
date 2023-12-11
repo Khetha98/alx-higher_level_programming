d053fd4a4c657a6a444add69214a2bea2f6ef4e4
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-12-12 01:32:44 +0200

    removed one space

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ from models.base import Base
 class Rectangle(Base):
     """It a rectangle class"""
 
-    def __init__(self, width, height, x=0, y=0, id=None): 
+    def __init__(self, width, height, x=0, y=0, id=None):
         """It a constructor"""
         self.width = width
         self.height = height
