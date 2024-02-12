0d36b568119ef890185029dac98f3beb8e47bc35
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-12 23:45:26 +0200

    empty line removal

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // loops through an array and print strings
-
 const listArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < listArray.length; i++) {
   console.log(listArray[i]);
