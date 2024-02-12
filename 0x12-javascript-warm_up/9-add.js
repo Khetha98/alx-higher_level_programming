fb5ac3557400b64ba6fd6edb8543a5e6a5dd37f6
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-13 00:12:13 +0200

    9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // Prints addition of 2 numbers
+const num1 = process.argv[2];
+const num2 = process.argv[3];
+
 function add (a, b) {
-    return a + b;
-  }
-  const arg = process.argv;
-  const number1 = parseInt(arg[2], 10);
-  const number2 = parseInt(arg[3], 10);
-  console.log(add(number1, number2));
+  return (a + b);
+}
+
+console.log(add(parseInt(num1), parseInt(num2)));
