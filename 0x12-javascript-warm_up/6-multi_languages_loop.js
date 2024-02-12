4bf6ce64a13fb76bbc4d7a569b991df12d0d3395
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-12 23:47:16 +0200

    changed approach

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // loops through an array and print strings
-const listArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < listArray.length; i++) {
-  console.log(listArray[i]);
-}
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+    console.log(line);
+  }
