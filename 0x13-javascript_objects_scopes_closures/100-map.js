02b377740ea51dec3d087bd354ec58b178a02dad
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-14 22:59:59 +0200

    task 4

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,8 +2,8 @@
 // It a script that imports an array.
 const data = require('./100-data');
 
-const originalList = data.list;
-const newList = originalList.map((value, index) => value * index);
+const original = data.list;
+const newList = original.map((value, index) => value * index);
 
-console.log(originalList);
+console.log(original);
 console.log(newList);
