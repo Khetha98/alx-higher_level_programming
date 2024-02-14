404e7a6ba8fe5daec939f65399a86b8d852453f9
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-14 22:45:53 +0200

    back shift

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,30 +1,30 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) { 
-        this.width = w;
-        this.height = h;
+  constructor (w, h) {
+    if (w > 0 && h > 0) { 
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
       }
+      if (i < this.height) { process.stdout.write('\n'); }
     }
+  }
 
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write('X');
-        }
-        if (i < this.height) { process.stdout.write('\n'); }
-      }
-    }
+  rotate () {
+    const temporaryHeight = this.height;
+    this.height = this.width;
+    this.width = temporaryHeight;
+  }
 
-    rotate () {
-      const temporaryHeight = this.height;
-      this.height = this.width;
-      this.width = temporaryHeight;
-    }
-
-    double () {
-      [this.width, this.height] = [this.width, this.height]
-        .map(prop => prop * 2);
-    }
+  double () {
+    [this.width, this.height] = [this.width, this.height]
+      .map(prop => prop * 2);
+  }
 };
