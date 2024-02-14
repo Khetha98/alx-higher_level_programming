1e7fc554532af849cbfddefbe3004774642dcc1b
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-14 22:16:12 +0200

    a space

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-// It returns a number of occurrences in the list
+
 exports.nbOccurences = function (list, searchElement) {
     let count = 0;
-  
     for (let idx = 0; idx < list.length; idx++) {
       if (list[idx] === searchElement) {
         count++;
       }
     }
     return count;
-  };
+};
