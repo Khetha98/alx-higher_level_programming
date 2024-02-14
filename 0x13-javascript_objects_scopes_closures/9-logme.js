ccb07abab8a188c0a7581fe83dceca115d095621
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-14 22:38:45 +0200

    space at begin

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// It gives a number of args already printed and a new arg value
+
 exports.logMe = function (item) {
   if (exports.logMe.count === undefined) {
     exports.logMe.count = 0;
