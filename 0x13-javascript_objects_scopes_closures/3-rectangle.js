e9e7b523e00cc7dec7d630cd3c78e8ec01509625
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-14 20:27:35 +0200

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if ((w > 0) && (h > 0)) {
-        [this.width, this.height] = [w, h];
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print () {
-      let size = this.height;
-      while (size > 0) {
-        console.log('X'.repeat(this.width));
-        size -= 1;
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
       }
+      if (i < this.height) { process.stdout.write('\n'); }
     }
+  }
 };
