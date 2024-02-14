d78bf8bc6f48caf8d6644802b98b4b745e149341
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-14 14:55:27 +0200

    added

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-
+/**
+ * Check the parameters provided
+ */
 class Rectangle {
     constructor (w, h) {
       if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
