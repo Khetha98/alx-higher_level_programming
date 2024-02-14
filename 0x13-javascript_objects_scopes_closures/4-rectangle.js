bb083eb0dfeb8a2c22086621a76568460105d268
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-14 22:52:40 +0200

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-
+// It a Rectangle class
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { 
+    if ((w = parseInt(w)) > 0 && (h = parseInt(h)) > 0) {
       this.width = w;
       this.height = h;
     }
@@ -15,9 +15,7 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    const temporaryHeight = this.height;
-    this.height = this.width;
-    this.width = temporaryHeight;
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
