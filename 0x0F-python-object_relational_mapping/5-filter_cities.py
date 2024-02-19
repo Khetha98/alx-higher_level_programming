7d0b5b11136fc0e03a17196899c071d821c2acc4
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-19 13:02:58 +0200

    changed var name

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,9 +5,9 @@ import sys
 
 
 if __name__ == "__main__":
-    db_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db_connect.cursor()
+    cur = db.cursor()
     cur.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name=%s""", (sys.argv[4],))
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     tmp = list(i[0] for i in rows)
     print(*tmp, sep=", ")
     cur.close()
-    db_connect.close()
+    db.close()
