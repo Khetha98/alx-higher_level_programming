1e5e18ea3888136a0477fb0e7e3a8ff9104a144a
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-19 14:24:32 +0200

    14 task

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -9,11 +9,18 @@ from model_city import City
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
+
+    results = session.query(City, State).join(State)
+
+    for city, state in results.all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.commit()
+    session.close()
