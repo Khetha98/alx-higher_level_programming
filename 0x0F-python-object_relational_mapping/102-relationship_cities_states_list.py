7b6bcb7fb40fe7effb7af4892cc505346c8f7cfb
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-23 23:46:47 +0200

    number 17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+""" prints State object with a name passed as the argument from database
 """
 import sys
 from relationship_state import Base, State
-from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
@@ -15,7 +13,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        for city_ins in instance.cities:
+    for i in session.query(State).order_by(State.id):
+        for city_ins in i.cities:
             print(city_ins.id, city_ins.name, sep=": ", end="")
-            print(" -> " + instance.name)
+            print(" -> " + i.name)
