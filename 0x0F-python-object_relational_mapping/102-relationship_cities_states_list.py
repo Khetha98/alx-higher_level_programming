6c11ddb409078df7ef0b6937c64132c8ec9ba386
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-23 23:47:38 +0200

    number 17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -3,8 +3,10 @@
 """
 import sys
 from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
