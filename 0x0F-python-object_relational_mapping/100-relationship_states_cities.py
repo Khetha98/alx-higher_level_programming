f09f1027e1a08de45a52e58d0e5ff73e2babbcfa
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-02-23 23:17:00 +0200

    double quotes

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,18 +2,18 @@
 """creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Access to the database"""
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(engine)
+    session = sessionmaker(bind=engine)
 
     theState = State(name='California')
     theCity = City(name='San Francisco')
