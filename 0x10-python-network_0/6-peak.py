fed26194ccb177c9fa96df4333cfd2d04133aa27
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-03-01 04:40:42 +0200

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Finds a peak in a list of unsorted integers
+""" It Finds the peak in a list of unsorted integers
 """
 
 
@@ -7,18 +7,18 @@ def find_peak(list_of_integers):
     """
     Args:
         list_of_integers(int): list of integers to find peak of
-    Returns: peak of list_of_integers or None
+    Returns: returns peak of list_of_integers or None
     """
-    size = len(list_of_integers)
-    mid_e = size
-    mid = size // 2
+    s = len(list_of_integers)
+    mid_e = s
+    mid = s // 2
 
-    if size == 0:
+    if s == 0:
         return None
 
     while True:
         mid_e = mid_e // 2
-        if (mid < size - 1 and
+        if (mid < s - 1 and
                 list_of_integers[mid] < list_of_integers[mid + 1]):
             if mid_e // 2 == 0:
                 mid_e = 2
