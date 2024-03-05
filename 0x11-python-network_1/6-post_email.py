fc7653d597ed3afe735ba979de5fb6809fe56cf4
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-03-06 01:36:36 +0200

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
-It a script that takes in a URL and an email address, sends a POST request to the
-passed URL with the email as a parameter, and finally displays the body
-of the response.
+Script that takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
 """
 import requests
-import sys
+from sys import argv
 
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-
-    r = requests.post(url, data=value)
+if __name__ == '__main__':
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
     print(r.text)
