36e1fe13fd50a0983203ca6d4294a0caa39e447b
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-03-06 02:08:37 +0200

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
-        if response == {}:
+        resp = req.json()
+        if resp == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("[{}] {}".format(resp.get("id"), resp.get("name")))
     except ValueError:
         print("Not a valid JSON")
