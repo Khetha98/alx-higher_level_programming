19c9db39fca5d4abe3b839923e23f6ce375769d3
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-03-06 02:06:59 +0200

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,8 +6,8 @@ import requests
 
 
 if __name__ == "__main__":
-    l = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": l}
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
