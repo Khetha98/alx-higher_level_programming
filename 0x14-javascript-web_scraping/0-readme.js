c3ef3d3290ffb0987412c21813a0c2744dbff150
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-03-27 04:38:43 +0200

    task 0 again

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-const file = process.argv[2];
+const f = require('fs');
+const thefile = process.argv[2];
 
-fs.readFile(file, 'utf-8', function (err, data) {
+f.readFile(thefile, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
